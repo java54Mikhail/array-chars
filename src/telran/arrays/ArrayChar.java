4b13f4fd8b77ab79ae8429526c78d48223380c78
MikhailTabulov <mtabulov2012@gmail.com>
2024-03-10 21:14:09 +0200

    ArrayChar compareTo test

---
+++ src/telran/arrays/ArrayChar.java
@@ -0,0 +1,69 @@
+package telran.arrays;
+
+import java.util.Iterator;
+
+public class ArrayChar {
+	   private char[] array;
+	   public ArrayChar(char[] array) {
+		  this.array = array;
+	   }
+	   public int compareTo(ArrayChar another) {
+		  int lenght = Math.min(this.array.length, another.array.length);
+		  int index = 0;
+		  while(index < lenght && this.array[index] == another.array[index]) {
+			  index++;
+		  }
+		   return index == lenght ? this.array.length - another.array.length : 
+			   this.array[index] - another.array[index];
+	   }
+	   public int indexOf(char character) {
+		int index = 0;
+		while(index < array.length && array[index] != character) {
+			index++;
+		}
+		   return index == array.length ? -1 : index;
+	}
+	   public int lastIndexOf(char character) {
+		   int index = array.length-1;
+			while(index > -1 && array[index] != character) {
+				index--;
+			}
+		   return index;
+	   }
+	public int count(char character) {
+		int index = 0;
+		int count = 0;
+		while (index < array.length) {
+			if (array[index] == character) {
+				count++;
+		}index++;
+		}
+		return count;
+	}
+	public int compareToIgnporeCase(ArrayChar another) {
+		  int lenght = Math.min(this.array.length, another.array.length);
+		  int index = 0;
+		  while(index < lenght && Character.toLowerCase(array[index]) == Character.toLowerCase(another.array[index])) {
+			  index++;
+		  }
+		   return index == lenght ? array.length - another.array.length : 
+			   Character.toLowerCase(array[index]) - Character.toLowerCase(another.array[index]);
+	}
+	public boolean contains(char character) {
+		for (int i = 0; i < array.length; i++) {
+			if(array[i] == character) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean equals(ArrayChar another) {
+		//TODO /each value chars of array?
+		return false;
+	}
+	public boolean equalsIgnoreCase(ArrayChar another) {
+		//TODO /depends from method equals
+		return false;
+	}
+	}
+
---
+++ src/telran/arrays/test/ArrayCharTest.java
@@ -0,0 +1,81 @@
+package telran.arrays.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.arrays.ArrayChar;
+
+class ArrayCharTest {
+
+	@Test
+	void compareToTest() {
+		char[] ar1 = {'1', '2', '3'};
+		char[] ar2 = {'9'};
+		char[] ar3 = {'1', '2', '3'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		ArrayChar arrayChar2 = new ArrayChar(ar2);
+		ArrayChar arrayChar3 = new ArrayChar(ar3);
+		assertEquals(8, arrayChar2.compareTo(arrayChar1));
+		assertEquals(-8, arrayChar1.compareTo(arrayChar2));
+		assertEquals(1, new ArrayChar(new char[] {'a', 'a'})
+				.compareTo(new ArrayChar(new char[] {'a'})));
+		assertEquals(-1, new ArrayChar(new char[] {'a'})
+				.compareTo(new ArrayChar(new char[] {'a', 'a'})));
+		assertEquals(0, arrayChar1.compareTo(arrayChar3));
+		}
+		@Test
+		void indexOfTest() {
+			char[] ar1 = {'1', '2', '3', '2'};
+			ArrayChar arrayChar1 = new ArrayChar(ar1);
+			assertEquals(1, arrayChar1.indexOf('2'));
+			assertEquals(-1, arrayChar1.indexOf('9'));
+		}
+		@Test
+		void lastIndexOfTest() {
+			char[] ar1 = {'1', '2', '3', '2'};
+			ArrayChar arrayChar1 = new ArrayChar(ar1);
+			assertEquals(3, arrayChar1.lastIndexOf('2'));
+			assertEquals(-1, arrayChar1.lastIndexOf('9'));
+		}
+		@Test
+		void countTest() {
+			char[] ar1 = {'1', '2', '3', '2'};
+			char[] ar2 = {'6', '4', '6', '0', '6','3'};
+			ArrayChar arrayChar1 = new ArrayChar(ar1);
+			ArrayChar arrayChar2 = new ArrayChar(ar2);
+			assertEquals(2, arrayChar1.count('2'));
+			assertEquals(3, arrayChar2.count('6'));
+			assertEquals(0, arrayChar1.count('m'));
+		}
+		@Test
+		void compareToIgnporeCaseTest() {
+			char[] ar1 = {'a', 'b', 'C'};
+			char[] ar2 = {'c'};
+			char[] ar3 = {'A', 'B', 'c'};
+			ArrayChar arrayChar1 = new ArrayChar(ar1);
+			ArrayChar arrayChar2 = new ArrayChar(ar2);
+			ArrayChar arrayChar3 = new ArrayChar(ar3);
+			assertTrue(arrayChar1.compareToIgnporeCase(arrayChar3) == 0);
+			assertEquals(0, arrayChar3.compareToIgnporeCase(arrayChar1));
+			assertEquals(0, arrayChar1.compareToIgnporeCase(arrayChar3));
+			assertEquals(2, arrayChar2.compareToIgnporeCase(arrayChar3));
+			assertEquals(2, arrayChar2.compareToIgnporeCase(arrayChar1));
+		}
+		@Test
+		void containsTest() {
+			char[] ar1 = {'1', '2', '3', '2'};
+			ArrayChar arrayChar1 = new ArrayChar(ar1);
+			assertTrue(arrayChar1.contains('3'));
+			assertFalse(arrayChar1.contains('0'));
+			assertFalse(arrayChar1.contains('-'));
+		}
+		@Test
+		void equalsTest() {
+			//TODO
+		}
+		@Test
+		void equalsIgnoreCaseTest() {
+			//TODO
+		}
+}
